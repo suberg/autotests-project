c1329ecea7b92e9a47b90d9ff5217bc54bb3cbe7
Anton Yakunov <Yakunov94@gmail.com>
2019-07-01 00:38:39 +0700

    Finding a bug on the page

---
+++ test_product_page.py
@@ -0,0 +1,12 @@
+import pytest
+from .pages.product_page import ProductPage
+
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_cart(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_add_item_to_basket()
