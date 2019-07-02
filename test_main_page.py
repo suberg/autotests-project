fd5006cb24bbe0c68a393e7d1bde1b2e5ddf2e3d
Anton Yakunov <Yakunov94@gmail.com>
2019-07-03 01:38:34 +0700

    Group login tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,23 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.cart_page import CartPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -19,8 +28,3 @@ def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
     cart_page.should_be_empty_basket_message()
     cart_page.should_not_be_basket_items()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
